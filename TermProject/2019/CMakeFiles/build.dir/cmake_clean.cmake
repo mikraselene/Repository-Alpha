0cdccd73824593af283c0b993d4a1f32034b872b
parvaluna <qyw_2000@163.com>
2020-06-09 10:24:21 +0800

    20200609

---
+++ TermProject/2019/CMakeFiles/build.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/build.dir/Labyrinthos.cpp.o"
+  "build"
+  "build.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/build.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
